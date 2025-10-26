7147ae6be70422f565ef1029f6955b4928a0d86e
thollarkings <thollarkings.ca@gmail.com>
2025-10-26 21:49:41 +0100

    fix: Use Hash History for Vue Router for GitHub Pages

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router' // <-- CHANGED IMPORT
 import LandingPage from '../views/LandingPage.vue'
 import Login from '../views/Login.vue'
 import Signup from '../views/Signup.vue'
@@ -34,7 +34,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // <-- CHANGED to use Hash History
   routes
 })
 
